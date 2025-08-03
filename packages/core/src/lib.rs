c03caf4a67702dfd76180cbf8ae783655ba25f71
akzhy <akshay48884@gmail.com>
2025-08-03 06:33:47 +0000

    Rename placeholder_image_kind to placeholder_type, add timing for transformation duration

--- packages/core/src/lib.rs
+++ packages/core/src/lib.rs
@@ -2,6 +2,9 @@
 
 pub mod placeholder_image;
 pub mod transform;
+pub mod store;
+
+use std::time::Instant;
 
 use napi_derive::napi;
 
@@ -10,14 +13,17 @@ use crate::transform::{TransformOptions, TransformOutput};
 #[tokio::main]
 #[napi]
 pub async fn transform(options: TransformOptions) -> Option<TransformOutput> {
-  transform::transform(transform::TransformOptions {
+  let instant = Instant::now();
+  let out = transform::transform(transform::TransformOptions {
     code: options.code,
     file_path: options.file_path,
-    placeholder_image_kind: options.placeholder_image_kind,
+    placeholder_type: options.placeholder_type,
     replace_function_call: options.replace_function_call,
     cache: options.cache,
     public_dir: options.public_dir,
     cache_file_dir: options.cache_file_dir,
   })
-  .await
+  .await;
+  println!("Transformation took {:?}", instant.elapsed());
+  out
 }
