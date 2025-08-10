1a1b2cb3225a88ca4655a003742ed8b0148ca023
akzhy <akshay48884@gmail.com>
2025-08-10 16:25:17 +0000

    Add basic webpack example

---
+++ examples/webpack-basic/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  devtool: "source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "@nocojs/webpack-loader",
+            options: {
+              publicDir: "public",
+              cacheFileDir: ".nocojs",
+              logLevel: "verbose",
+              placeholderType: "blurred",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+};
