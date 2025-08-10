69b4ff1ff14df894474bdc43eb232884451b3b9c
akzhy <akshay48884@gmail.com>
2025-08-10 16:25:35 +0000

    Add webpack react example

---
+++ examples/webpack-react/src/App.js
@@ -0,0 +1,46 @@
+import { preview } from "@nocojs/client";
+import { useState } from "react";
+import "./App.css";
+
+const images = [
+  {
+    preview: preview(
+      "https://images.unsplash.com/photo-1506905925346-21bda4d32df4"
+    ),
+    src: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4",
+  },
+  {
+    preview: preview(
+      "https://images.unsplash.com/photo-1518837695005-2083093ee35b"
+    ),
+    src: "https://images.unsplash.com/photo-1518837695005-2083093ee35b",
+  },
+  {
+    preview: preview(
+      "https://images.unsplash.com/photo-1511593358241-7eea1f3c84e5"
+    ),
+    src: "https://images.unsplash.com/photo-1511593358241-7eea1f3c84e5",
+  },
+];
+
+function App() {
+  const [hovered, setHovered] = useState(null);
+
+  return (
+    <div className="App">
+      <div className="image-gallery">
+        {images.map((image) => (
+          <img
+            key={image.src}
+            src={hovered === image.src ? image.src : image.preview}
+            alt=""
+            onMouseEnter={() => setHovered(image.src)}
+            onMouseLeave={() => setHovered(null)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
