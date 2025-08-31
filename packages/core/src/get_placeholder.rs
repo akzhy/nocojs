73170149c9eb6f155595b43ee560901e7b0675ac
akzhy <akshay48884@gmail.com>
2025-08-31 10:24:35 +0000

    Update logic and input type for get_placeholder

--- packages/core/src/get_placeholder.rs
+++ packages/core/src/get_placeholder.rs
@@ -8,22 +8,27 @@ use rusqlite::{params, Connection};
 use url::Url;
 
 use crate::{
-  log::{create_log, style_error, LogLevel},
-  placeholder_image::{process_image, PlaceholderImageOutputKind},
+  log::{self, collect_logs, create_log, style_error, LogLevel},
+  placeholder_image::{process_image, wrap_with_svg, PlaceholderImageOutputKind},
   store::Store,
   transform::{init_cache_dir, setup_sqlite, PreviewOptions, RUSQLITE_FILE_NAME},
 };
 
 #[napi(object)]
 pub struct GetPlaceholderOptions {
-  pub preview_options: PreviewOptions,
+  pub width: Option<u32>,
+  pub height: Option<u32>,
+  pub output_kind: Option<PlaceholderImageOutputKind>,
   pub cache_file_dir: Option<String>,
-  pub public_dir: Option<String>,
+  pub cache: Option<bool>,
+  pub wrap_with_svg: Option<bool>,
 }
 
 #[napi(object)]
 pub struct GetPlaceholderOutput {
   pub placeholder: String,
+  pub logs: Vec<log::Log>,
+  pub is_error: bool,
 }
 
 static HTTP_CLIENT: Lazy<Client> = Lazy::new(|| {
@@ -45,24 +50,62 @@ pub async fn get_placeholder(
   )
   .unwrap_or("".to_string());
 
+  let preview_options = PreviewOptions {
+    width: options.width,
+    height: options.height,
+    output_kind: options
+      .output_kind
+      .clone()
+      .unwrap_or(PlaceholderImageOutputKind::Normal),
+    cache: options.cache.unwrap_or(true),
+    wrap_with_svg: options.wrap_with_svg.unwrap_or(true),
+    replace_function_call: false,
+  };
+
   let db_filepath = PathBuf::from(&cache_dir).join(RUSQLITE_FILE_NAME);
   let conn = Connection::open(&db_filepath).unwrap();
 
   let _ = setup_sqlite(&conn);
 
-  if let Some(placeholder) = check_cache(url.clone(), &options.preview_options, &conn) {
-    return Ok(GetPlaceholderOutput { placeholder });
+  if let Ok((placeholder, original_width, original_height)) =
+    check_cache(url.clone(), &preview_options, &conn)
+  {
+    create_log(format!("Cache hit for {}", url), LogLevel::Info);
+
+    return Ok(GetPlaceholderOutput {
+      placeholder: if preview_options.wrap_with_svg {
+        wrap_with_svg(placeholder, original_width, original_height)
+      } else {
+        placeholder
+      },
+      logs: collect_logs(),
+      is_error: false,
+    });
   }
 
-  let bytes = get_bytes(url.clone(), &options).await.unwrap();
+  let bytes = get_bytes(url.clone()).await?;
 
-  match process_image(&bytes, &url, &options.preview_options).await {
+  match process_image(&bytes, &url, &preview_options).await {
     Ok(out) => {
-      let cache_key = Store::create_cache_key(&options.preview_options);
+      if !preview_options.cache {
+        return Ok(GetPlaceholderOutput {
+          placeholder: {
+            if preview_options.wrap_with_svg {
+              wrap_with_svg(out.base64_str, out.original_width, out.original_height)
+            } else {
+              out.base64_str
+            }
+          },
+          logs: collect_logs(),
+          is_error: false,
+        });
+      }
+
+      let cache_key = Store::create_cache_key(&preview_options);
       let to_insert = (
         url.clone(),
         out.base64_str.clone(),
-        PlaceholderImageOutputKind::get_string_name(&options.preview_options.output_kind.clone()),
+        PlaceholderImageOutputKind::get_string_name(&preview_options.output_kind.clone()),
         cache_key.clone(),
         out.original_width,
         out.original_height,
@@ -81,7 +124,13 @@ pub async fn get_placeholder(
       )?;
 
       Ok(GetPlaceholderOutput {
-        placeholder: out.base64_str,
+        placeholder: if preview_options.wrap_with_svg {
+          wrap_with_svg(out.base64_str, out.original_width, out.original_height)
+        } else {
+          out.base64_str
+        },
+        logs: collect_logs(),
+        is_error: false,
       })
     }
     Err(e) => {
@@ -94,24 +143,29 @@ pub async fn get_placeholder(
   }
 }
 
-fn check_cache(url: String, preview_options: &PreviewOptions, conn: &Connection) -> Option<String> {
+fn check_cache(
+  url: String,
+  preview_options: &PreviewOptions,
+  conn: &Connection,
+) -> Result<(String, u32, u32), Box<dyn std::error::Error>> {
   let cache_key = Store::create_cache_key(preview_options);
-  let sql = "SELECT id, placeholder FROM images WHERE url = ? AND cache_key = ?";
+  let sql = "SELECT id, placeholder, original_width, original_height FROM images WHERE url = ? AND cache_key = ?";
   let params = params![url, cache_key];
-  conn.query_row(sql, params, |row| row.get(1)).ok()
+  let result = conn.query_row(sql, params, |row| {
+    let placeholder: String = row.get(1)?;
+    let original_width: u32 = row.get(2)?;
+    let original_height: u32 = row.get(3)?;
+    Ok((placeholder, original_width, original_height))
+  })?;
+
+  Ok(result)
 }
 
-async fn get_bytes(
-  url: String,
-  options: &GetPlaceholderOptions,
-) -> Result<Bytes, Box<dyn std::error::Error>> {
+async fn get_bytes(url: String) -> Result<Bytes, Box<dyn std::error::Error>> {
   let url_parse = Url::parse(&url);
 
   if url_parse.is_err() {
-    // Assumes the URL is a relative path to an image in the public directory
-    let public_dir = options.public_dir.clone().unwrap_or("public".to_string());
-    let relative_url = url.strip_prefix("/").unwrap_or(&url);
-    let image_path = PathBuf::from(public_dir.clone()).join(relative_url);
+    let image_path = PathBuf::from(url);
 
     if !image_path.exists() {
       return Err("Image not found".into());
@@ -120,18 +174,15 @@ async fn get_bytes(
     let file_read = std::fs::read(image_path.as_path());
     if file_read.is_err() {
       create_log(
-        style_error(format!(
-          "Failed to read image from public directory: {:?} {:?}",
-          image_path, public_dir
-        )),
+        style_error(format!("Failed to read image from path: {:?}", image_path)),
         LogLevel::Error,
       );
-      return Err("Failed to read image from public directory".into());
+      return Err("Failed to read image from path".into());
     }
     let bytes = Bytes::from(file_read.unwrap());
-    return Ok(bytes);
+    Ok(bytes)
   } else {
     let bytes = HTTP_CLIENT.get(url).send().await?.bytes().await?;
-    return Ok(bytes);
+    Ok(bytes)
   }
 }
