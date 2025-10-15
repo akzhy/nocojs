ea715467b0ceb140f8699f12994a305829779727
Akshay <akshay48884@gmail.com>
2025-10-15 22:42:42 +0530

    Add timeout to image

--- packages/core-js/src/image.ts
+++ packages/core-js/src/image.ts
@@ -7,7 +7,9 @@ export const getSharpInstance = async (
     if (!isValidUrl(urlOrPath)) {
       throw new Error(`Invalid URL: ${urlOrPath}`);
     }
-    const response = await fetch(urlOrPath);
+    const response = await fetch(urlOrPath, {
+      signal: AbortSignal.timeout(10000),
+    });
     const buffer = await response.arrayBuffer();
     const image = sharp(Buffer.from(buffer));
     return image;
