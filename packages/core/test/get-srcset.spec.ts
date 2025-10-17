0210c0b63fa7a5307fa7f012a4190e4ff5712e05
Akshay <akshay48884@gmail.com>
2025-10-17 22:39:00 +0530

    Add basic test for getSrcset

---
+++ packages/core/test/get-srcset.spec.ts
@@ -0,0 +1,35 @@
+import path, { dirname } from "path";
+import { describe, expect, test } from "vitest";
+import { defaultTransformOptions } from "./utils";
+import { GetSrcsetOptions, getSrcset } from "../src/get-srcset";
+import { fileURLToPath } from "url";
+import { existsSync } from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+describe("getSrcset function tests", () => {
+  const testImagePath = path.join(
+    defaultTransformOptions.publicDir!,
+    "good_boy_4x5.jpg"
+  );
+  const baseOptions: GetSrcsetOptions = {
+    outputDir: path.join(__dirname, "public", "get_srcset"),
+  };
+
+  test("should generate srcset local image file", async () => {
+    const result = await getSrcset(testImagePath, baseOptions);
+    console.log(result);
+    expect(result.images.length).toBeGreaterThan(0);
+
+    result.images.forEach((img) => {
+      expect(typeof img.src).toBe("string");
+      expect(typeof img.width).toBe("number");
+      expect(typeof img.height).toBe("number");
+      expect(typeof img.format).toBe("string");
+      expect(typeof img.filePath).toBe("string");
+
+      expect(existsSync(img.filePath)).toBe(true);
+    });
+  });
+});
